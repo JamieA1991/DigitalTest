cc83ae351ec10bf988c88413be4c065489c9572e
JamieA1991 <jamie.airey@ukpowernetworks.co.uk>
2025-08-28 17:36:03 +0100

    Add project files.

---
+++ helloworld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("I am learning C#");//test
+        Console.WriteLine("Isn't that Cool?");
+        Console.WriteLine(3 + 3);
+    }
+}
