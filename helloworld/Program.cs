9af41948526692e42f70477b587d2a2bab5e4e01
Jamie Airey <jamie.airey@ukpowernetworks.co.uk>
2025-08-28 17:46:42 +0100

    Variables

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main()
     {
+        int num = 15;
+        Console.WriteLine("int: " + num);
+        num = 20;
+        Console.WriteLine("int: " + num);
+
+        double dnum = 5.99D;
+        Console.WriteLine("Double: " + dnum);
+
+        char letter = 'd';
+        Console.WriteLine("char: " + letter);
+
+        bool mybool = false;
+        Console.WriteLine("booleen: " + mybool);
+        string name = "Jamie";
+        Console.WriteLine("String: " + name);
+
+
+
+
+
+
+        /* this part of the code means nothing */
         Console.WriteLine("Hello, World!");
         Console.WriteLine("I am learning C#");//test
         Console.WriteLine("Isn't that Cool?");
